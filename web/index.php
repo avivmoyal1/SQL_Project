254082c3ca1201bee394ab477a91f3000cebce10
avivmoyal1 <avivmoyal1@gmail.com>
2022-07-26 23:09:01 +0300

    aviv

--- web/index.php
+++ web/index.php
@@ -23,16 +23,18 @@
 <html>
 
 <head>
-    <title>SafeGame</title>
+    <title>SQL</title>
     <meta charset="UTF-8">
     <!-- my-css -->
     <link rel="stylesheet" href="style.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 
 
-</head>
+</head >
 
-<body>
+<body class="container">
         <header>
         </header>
         <main>
@@ -51,21 +53,24 @@
                 <button type="submit" value="Submit" >Submit</button>
             </form>
 
-            <div>
+            
                 
               <?php
+
                     $op = 0;
+                    $flag = 0;
                     if(isset($_GET['option'])){
                         $op = $_GET['option'];
                     }
                     switch ($op){
                         case 1:
-                            echo "<section>product name<span> </span><span>amount</span></section>";
+                           
+                            echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> product name </div><div class='col-sm-2'> amount</div></div></div></b>";
                             $query = "SELECT product_name, amount from product";
                             $result = mysqli_query($connection,$query); 
                             while($row = mysqli_fetch_array($result)){
-                                echo "<section><span>" . $row['product_name'] ."</span> <span>" . $row['amount'] . "</span></section>";
-                     }
+                                echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'>" . $row['product_name'] ."</div> <div class='col-sm-2'>" . $row['amount'] . "</div></div></div>";
+                            }
                             break;
                         case 2:
                             echo '<form action="#" method="GET">';
@@ -75,11 +80,11 @@
                             echo '</form>';
                             
                             if(isset($_GET['weeks'])){
-                                echo "<section><span> order_id </span> <span> customer_id </span> <span> member_id </span> <span> delivery </span> <span> product_id  </span> <span> price </span> <span> order_date </span></section>";
+                                echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-1'> order_id </div> <div class='col-sm-1'> customer_id </div> <div class='col-sm-1'> member_id </div> <div class='col-sm-1'> delivery </div> <div class='col-sm-1'> product_id  </div> <div class='col-sm-1'> price </div> <div class='col-sm-1'> order_date </div></div></div></b>";
                                 $query = "SELECT * FROM store_order WHERE order_date >= curdate() - INTERVAL ".$_GET['weeks'] ." WEEK";
                                 $result = mysqli_query($connection,$query); 
                                 while($row = mysqli_fetch_array($result)){
-                                    echo "<section><span>" . $row['order_id'] ."</span> <span>" . $row['customer_id'] . "</span> <span>" . $row['member_id'] . "</span> <span>" . $row['delivery'] . "</span> <span> " . $row['product_id'] . " </span> <span>" . $row['price'] . "</span> <span> " . $row['order_date'] . "</span></section>";
+                                    echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-1'>" . $row['order_id'] ."</div> <div class='col-sm-1'>" . $row['customer_id'] . "</div> <div class='col-sm-1'>" . $row['member_id'] . "</div> <div class='col-sm-1'>" . $row['delivery'] . "</div> <div class='col-sm-1'> " . $row['product_id'] . " </div> <div class='col-sm-1'>" . $row['price'] . "</div> <div class='col-sm-1'> " . $row['order_date'] . "</div></div></div>";
                                 }
                             }
                             break;
@@ -95,10 +100,10 @@
                             ORDER BY amount DESC LIMIT 1";
                             $result = mysqli_query($connection,$query); 
 
-                            echo "<section><span> member_id </span> <span> name </span> <span> amount </span> </section>";
+                            echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> member_id </div> <div class='col-sm-2'> name </div> <div class='col-sm-2'> amount </div> </div></div></b>";
 
                             while($row = mysqli_fetch_array($result)){
-                                echo "<section><span>" . $row['member_id'] ."</span> <span>" . $row['p_name'] . "</span> <span>" . $row['amount'] . "</span> </section>";
+                                echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> " . $row['member_id'] ."</div> <div class='col-sm-2'>" . $row['p_name'] . "</div> <div class='col-sm-2'>" . $row['amount'] . "</div></div> </div>";
                             }
                             break;
                         case 4:
@@ -113,10 +118,10 @@
                             ORDER BY total DESC LIMIT 1";
                             $result = mysqli_query($connection,$query); 
 
-                            echo "<section><span> member id </span> <span> name </span> <span> total </span> </section>";
+                            echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> member id </div> <div class='col-sm-2'> name </div> <div class='col-sm-2'> total </div> </div></div></b>";
 
                             while($row = mysqli_fetch_array($result)){
-                                echo "<section><span>" . $row['member_id'] ."</span> <span>" . $row['p_name'] . "</span> <span>" . $row['total'] . "</span> </section>";
+                                echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'>" . $row['member_id'] ."</div> <div class='col-sm-2'>" . $row['p_name'] . "</div> <div class='col-sm-2'>" . $row['total'] . "</div></div> </div>";
                             }
                             break;
                         case 5:
@@ -129,12 +134,16 @@
                                 s.customer_id = c.customer_id
                             where s.delivery IS NULL";
                             $result = mysqli_query($connection,$query); 
-
-                            echo "<section><span> customer id </span> <span> name </span> <span> order id </span> <span> product id </span> <span> price </span></section>";
-
+                            echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> customer id </div> <div class='col-sm-2'> name </div> <div class='col-sm-2'> order id </div> <div class='col-sm-2'> product id </div> <div class='col-sm-2'> price </div></div></div></b>";
                             while($row = mysqli_fetch_array($result)){
-                                echo "<section><span>" . $row['customer_id'] ."</span> <span>" . $row['p_name'] . "</span> <span>" . $row['order_id'] . "</span> <span>" .$row['product_id']. "</span> <span>" .$row['price']." </span></section>";
+                                echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'>" . $row['customer_id'] ."</div> <div class='col-sm-2'>" . $row['p_name'] . "</div> <div class='col-sm-2'>" . $row['order_id'] . "</div> <div class='col-sm-2'>" .$row['product_id']. "</div> <div class='col-sm-2'>" .$row['price']." </div></div</div>";
+                                $flag++;
                             }
+                            if($flag == 0){
+                                echo "balalbablabl";
+                                $flag =0;
+                            }
+                            
                             break;
                         case 6:
                             $query = "SELECT  c.customer_id, p.p_name FROM person p	
@@ -147,10 +156,10 @@
                             WHERE s.customer_id IS NULL";
                             $result = mysqli_query($connection,$query); 
 
-                            echo "<section><span> customer id </span> <span> name </span> </section>";
+                            echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> customer id </div> <div class='col-sm-2'> name </div></div></div></b>";
 
                             while($row = mysqli_fetch_array($result)){
-                                echo "<section><span>" . $row['customer_id'] ."</span> <span>" . $row['p_name'] . " </span></section>";
+                                echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'>" . $row['customer_id'] ."</div> <div class='col-sm-2'>" . $row['p_name'] . " </div></div></div>";
                             }
                             break;
                         case 7:
@@ -166,10 +175,10 @@
                             ORDER BY delivery_amount DESC";
                             $result = mysqli_query($connection,$query); 
 
-                            echo "<section><span> customer id </span> <span> name </span><span> order amount </span> </section>";
+                            echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> customer id </div> <div class='col-sm-2'> name </div><div class='col-sm-2'> order amount </div></div></div></b>";
 
                             while($row = mysqli_fetch_array($result)){
-                                echo "<section><span>" . $row['customer_id'] ."</span> <span>" . $row['p_name'] ."</span> <span>" . $row['delivery_amount'] . " </span></section>";
+                                echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'>" . $row['customer_id'] ."</div> <div class='col-sm-2'>" . $row['p_name'] ."</div> <div class='col-sm-2'>" . $row['delivery_amount'] . " </div></div></div>";
                             }
                             break;
                         case 8:
@@ -180,13 +189,13 @@
                             echo '</form>';
                             
                             if(isset($_GET['month'])){
-                                echo "<section><span> revenues </span></section>";
+                                echo "<b><div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'> revenues </div></div></div></b>";
                                 $query = "SELECT sum(price) as revenues FROM store_order 
                                 WHERE order_date >= curdate() - INTERVAL " .$_GET['month']. " month"; 
                                 
                                 $result = mysqli_query($connection,$query); 
                                 while($row = mysqli_fetch_array($result)){
-                                    echo "<section><span>" . $row['revenues'] ."</span></section>";
+                                    echo "<div class='container text-center'><div class='row row-cols-auto'><div class='col-sm-2'>" . $row['revenues'] ."</div></div></div>";
                                 }
                             }
                             break;
@@ -202,7 +211,7 @@
                     // }
                 ?>
           
-            </div>
+     
         </main>
 </body>
 </html>
