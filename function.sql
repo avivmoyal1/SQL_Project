3d90b42f0a68c2c00ab926bd20a7205310f699cc
avivmoyal1 <avivmoyal1@gmail.com>
2022-08-02 18:47:38 +0300

    AVIV

--- function.sql
+++ function.sql
@@ -1,19 +1,19 @@
 SET GLOBAL log_bin_trust_function_creators = 1;
-DELIMITER $$ 
+DELIMITER $$
+
+drop function  employee_income;
 CREATE FUNCTION 
 	employee_income(in_name VARCHAR(100), in_month INT, in_year INT) RETURNS INTEGER
 BEGIN
 		DECLARE e_count INTEGER DEFAULT 0;
-        SELECT sum(price) INTO e_count FROM store_order INNER JOIN crew 
+        SELECT sum(price) INTO e_count FROM store_order s
+        INNER JOIN crew c
 			ON
-                store_order.member_id = crew.member_id
-		INNER JOIN person
+                s.member_id = c.member_id
+		INNER JOIN person p
 			ON
-				person.person_id = crew.person_id
-		INNER JOIN delivery
-			ON
-				delivery.delivery_id = store_order.delivery
-		WHERE person.p_name = in_name and MONTH(delivery.order_date) = in_month and YEAR(delivery.order_date) = in_year;
+				p.person_id = c.person_id
+		WHERE p.p_name = in_name and MONTH(s.order_date) = in_month and YEAR(s.order_date) = in_year;
         
 		RETURN e_count;
 END; $$
